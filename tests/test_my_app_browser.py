5bdb61e79e62465fcfa52f8ccca8ae9cf422fdc3
Sarah Nicholson <sarah.nicholson.13@ucl.ac.uk>
2021-02-20 13:45:23 +0000

    added a wait to allow the server to start before running the selenium tests

--- tests/test_my_app_browser.py
+++ tests/test_my_app_browser.py
@@ -4,6 +4,7 @@ import pytest
 @pytest.mark.usefixtures('chrome_driver', 'run_app')
 class TestMyAppBrowser:
     def test_app_is_running(self, app):
+        self.driver.implicitly_wait(20)
         self.driver.get("http://127.0.0.1:5000/")
         assert self.driver.title == 'Home page'
 
